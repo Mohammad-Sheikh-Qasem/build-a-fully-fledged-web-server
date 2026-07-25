79899e72dc31693a7b0230dd46396e044d30d27d
Mohammad sheikh Qasem <mohamad.sheikh.qasem.cs.e@gmail.com>
2026-07-25 13:31:04 +0300

    add chirps table schema and generated migrations

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,13 +1,18 @@
-import { pgTable, timestamp, varchar, uuid } from "drizzle-orm/pg-core";
+import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
   id: uuid("id").primaryKey().defaultRandom(),
   createdAt: timestamp("created_at").notNull().defaultNow(),
-  updatedAt: timestamp("updated_at")
-    .notNull()
-    .defaultNow()
-    .$onUpdate(() => new Date()),
-  email: varchar("email", { length: 256 }).unique().notNull(),
+  updatedAt: timestamp("updated_at").notNull().defaultNow(),
+  email: text("email").notNull().unique(),
 });
 
-export type NewUser = typeof users.$inferInsert;
+export const chirps = pgTable("chirps", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at").notNull().defaultNow(),
+  body: text("body").notNull(),
+  userId: uuid("user_id")
+    .notNull()
+    .references(() => users.id, { onDelete: "cascade" }),
+});
