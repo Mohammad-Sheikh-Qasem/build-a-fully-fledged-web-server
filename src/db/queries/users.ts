6fd0601fdc734b7c478f84a05d63edb577e9afc5
Mohammad sheikh Qasem <mohamad.sheikh.qasem.cs.e@gmail.com>
2026-07-25 13:32:55 +0300

    update user query types and imports

--- src/db/queries/users.ts
+++ src/db/queries/users.ts
@@ -1,15 +1,14 @@
 import { db } from "../index.js";
-import { NewUser, users } from "../schema.js";
+import { users } from "../schema.js";
+import { InferInsertModel } from "drizzle-orm";
+
+export type NewUser = InferInsertModel<typeof users>;
 
 export async function createUser(user: NewUser) {
-  const [result] = await db
-    .insert(users)
-    .values(user)
-    .onConflictDoNothing()
-    .returning();
-
-  return result;
+  const [newUser] = await db.insert(users).values(user).returning();
+  return newUser;
 }
+
 export async function resetUsers() {
   await db.delete(users);
 }
