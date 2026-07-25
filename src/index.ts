a4bd91f4fdcafd1142e8ec99af10751281b0128a
Mohammad sheikh Qasem <mohamad.sheikh.qasem.cs.e@gmail.com>
2026-07-25 12:51:04 +0300

    execute automatic database migrations on server start

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
 import express from "express";
 import { Request, Response, NextFunction } from "express";
 import { apiConfig } from "./config.js";
+import postgres from "postgres";
+import { migrate } from "drizzle-orm/postgres-js/migrator";
+import { drizzle } from "drizzle-orm/postgres-js";
+import { config } from "./config.js";
+
+const migrationClient = postgres(config.db.url, { max: 1 });
+await migrate(drizzle(migrationClient), config.db.migrationConfig);
+
+
 
 export class BadRequestError extends Error {
   constructor(message: string) {
