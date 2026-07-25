996f10f96fff92728abc09ffa96ee9f11f3446f5
Mohammad sheikh Qasem <mohamad.sheikh.qasem.cs.e@gmail.com>
2026-07-25 12:37:09 +0300

    add dbURL to config and ignore .env file

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,23 @@
+import process from "node:process";
+
+process.loadEnvFile();
+
+function envOrThrow(key: string): string {
+  const value = process.env[key];
+  if (!value) {
+    throw new Error(`Missing environment variable: ${key}`);
+  }
+  return value;
+}
+
 export type APIConfig = {
   fileserverHits: number;
+  dbURL: string;
 };
 
-export const apiConfig: APIConfig = {
+export const config: APIConfig = {
   fileserverHits: 0,
+  dbURL: envOrThrow("DB_URL"),
 };
+
+export const apiConfig = config;
