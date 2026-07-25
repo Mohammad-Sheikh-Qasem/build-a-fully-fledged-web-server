9156ac6f2cbc1a488037fc105f864c04942ad3f5
Mohammad sheikh Qasem <mohamad.sheikh.qasem.cs.e@gmail.com>
2026-07-25 13:41:30 +0300

    add getAllChirps query ordered by createdAt ascending

--- src/db/queries/chirps.ts
+++ src/db/queries/chirps.ts
@@ -1,5 +1,6 @@
 import { db } from "../index.js";
 import { chirps } from "../schema.js";
+import { asc } from "drizzle-orm";
 
 export async function createChirp(data: { body: string; userId: string }) {
   const [newChirp] = await db
@@ -11,4 +12,11 @@ export async function createChirp(data: { body: string; userId: string }) {
     .returning();
 
   return newChirp;
+}
+
+export async function getAllChirps() {
+  return await db
+    .select()
+    .from(chirps)
+    .orderBy(asc(chirps.createdAt));
 }
