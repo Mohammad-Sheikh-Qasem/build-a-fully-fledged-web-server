ea328d0f45d7ee44d21c7e59ddfa10b1855b0462
Mohammad sheikh Qasem <mohamad.sheikh.qasem.cs.e@gmail.com>
2026-07-26 11:46:55 +0300

    add hashed_password column to users schema and migrations

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,10 +1,11 @@
-import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+import { pgTable, text, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
   id: uuid("id").primaryKey().defaultRandom(),
   createdAt: timestamp("created_at").notNull().defaultNow(),
   updatedAt: timestamp("updated_at").notNull().defaultNow(),
   email: text("email").notNull().unique(),
+  hashedPassword: varchar("hashed_password").notNull().default("unset"),
 });
 
 export const chirps = pgTable("chirps", {
